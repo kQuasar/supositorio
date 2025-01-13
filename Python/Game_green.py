019a23acb476fad85f7f13aba2160175e9851193
Ricardo H. <henrique.sousas100@gmail.com>
2025-01-13 20:22:22 -0300

    descrição da atividade

---
+++ Python/Game_green.py
@@ -0,0 +1,3 @@
+#Crie um jogo aonde o fundo é verde e o personagem do jogo é um quadrado vermelho.
+#Adicione a ele a capacidade de se movimentar.
+#E adicione uma tecla que quando clicada mnude a cor do quadrado para uma aleatoria.
