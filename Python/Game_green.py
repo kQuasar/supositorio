bb63f7e6da62a058d9c3b228a2ac1f75bc84bf6b
kQuasar <pablohenri0220@gmail.com>
2025-01-15 20:54:18 -0300

    verde maconha

--- Python/Game_green.py
+++ Python/Game_green.py
@@ -16,15 +16,27 @@ class Quadrado (pygame.sprite.Sprite):
         self.homofobico = (self.largura / 2, self.altura / 2)
         self.image = pygame.Surface(size = self.homofobico)
         self.image.fill(self.cor_quadrado)
-        self.rect = self.image.get_rect(center = (50, 50))
+        self.rect = self.image.get_rect(center = (425, 350))
 
     def update(self):
-        pass
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= 2
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += 2
+        if keys[pygame.K_DOWN]:
+            self.rect.y += 2
+        if keys[pygame.K_UP]:
+            self.rect.y -= 2
+
+                
+
+        
 
 pygame.init() 
 
 vermelho = (245, 0 , 35)
-verde = (25, 255, 0)
+verde = (0, 155, 0)
 azul = (50, 150, 245)
 
 largura = 850
@@ -43,6 +55,8 @@ corrida = True
 
 while corrida:
     for event in pygame.event.get():
+        if event.type == pygame.K_SPACE:
+                variavel.image.fill((random.randint(1, 255),random.randint(1, 255),random.randint(1, 255)))
         if event.type == pygame.QUIT:
             corrida = False
 
