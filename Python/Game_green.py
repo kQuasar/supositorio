a26a5d800d98c400ad69305b938147d9d630a77f
kQuasar <pablohenri0220@gmail.com>
2025-01-14 21:19:34 -0300

    alguma coisa

--- Python/Game_green.py
+++ Python/Game_green.py
@@ -6,7 +6,20 @@ import pygame
 import pygame.display
 import random
 
-pygame.init()
+class Quadrado (pygame.sprite.Sprite):
+    def init(self):
+        super().init()
+        self.cor_quadrado = (250, 95, 95)
+        self.largura = 50
+        self.altura = 50
+        self.gay_tamanho = 30
+        self.homofobico = (self.largura / 2, self.altura / 2)
+        self.image = pygame.Surface(self.homofobico)
+        self.image.fill(self.cor_quadrado)
+        self.rect =self.image.get_rect(center = (50, 50))
+        
+
+pygame.init() 
 
 vermelho = (245, 0 , 35)
 verde = (25, 255, 0)
@@ -15,14 +28,14 @@ azul = (50, 150, 245)
 largura = 850
 altura = 700
 janela = pygame.display.set_mode((largura, altura))
-pygame.display.set_caption("jogo do quadrado vermelhão")
-
-gay_pos = [largura / 2, altura / 2]
-gay_tamanho = 30
-
-cor_quadrado = vermelho
+pygame.display.set_caption("jogo do quadrado vermelhão") 
 
 font = pygame.font.Font(None, 36)
+
+variavel = Quadrado()
+
+valor = pygame.sprite.Group()
+valor.add(variavel)
 
 corrida = True
 
@@ -31,29 +44,11 @@ while corrida:
         if event.type == pygame.QUIT:
             corrida = False
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        gay_pos[0] -= 1
-    if keys[pygame.K_RIGHT]:
-        gay_pos[0] += 1
-    if keys[pygame.K_UP]:
-        gay_pos[1] -= 1
-    if keys[pygame.K_DOWN]:
-        gay_pos[1] += 1
+    
 
-    gay_pos[0] = max(0, min(largura - gay_tamanho, gay_pos[0]))
-    gay_pos[1] = max(0, min(altura - gay_tamanho, gay_pos[1]))
-
-    if keys[pygame.K_SPACE]:
-        cor_quadrado = (
-            random.randint(0, 255),
-            random.randint(0, 255),
-            random.randint(0,255)
-        )
-
+    
     janela.fill(verde)
-
-    pygame.draw.rect(janela, cor_quadrado, (*gay_pos, gay_tamanho, gay_tamanho))
+    valor.draw(janela)
 
     pygame.display.flip()
 
